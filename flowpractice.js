947a454d1912590d16e4969009385680649a6555
Fern Piya S <125846412+fernfrn@users.noreply.github.com>
2023-03-13 14:00:37 +0700

    Create flowpractice.js
    submit homework

---
+++ flowpractice.js
@@ -0,0 +1,13 @@
+function dayNumber(number) {
+    switch (number) {
+        case 0: return "Sunday";
+        case 1: return "Monday";
+        case 2: return "Tuesday";
+        case 3: return "Wednesday";
+        case 4: return "Thursday";
+        case 5: return "Friday";
+        case 6: return "Saturday";
+}
+}
+console.log(dayNumber(0));
+
